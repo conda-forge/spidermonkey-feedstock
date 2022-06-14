5b20f27d0d97865aabdde6824737cd5c5425a176
Pepijn de Vos <pepijndevos@gmail.com>
2022-06-14 10:41:08 +0200

    try mach

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,29 +1,11 @@
-set LIBXUL_DIST=$out
-set M4=m4
-set AWK=awk
-set LLVM_OBJDUMP=objdump
-set CPPFLAGS="-D__STDC_FORMAT_MACROS $CPPFLAGS"
+dir
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 @rem We can't build in js/src/, so create a build dir
 mkdir obj
-cd obj/
-python ../configure.py ^
-       --prefix=$PREFIX ^
-       --enable-project=js ^
-       --disable-ctypes ^
-       --disable-jit ^
-       --disable-jemalloc ^
-       --enable-optimize ^
-       --enable-hardening ^
-       --with-intl-api ^
-       --build-backends=RecursiveMake ^
-       --with-system-icu ^
-       --disable-debug ^
-       --enable-gczeal ^
-       --enable-strip ^
-       --disable-install-strip
-if errorlevel 1 exit 1
+copy .mozconfig obj/.mozconfig
 
-make -j$CPU_COUNT
+cd obj/
+python ../mach.py build
 if errorlevel 1 exit 1
