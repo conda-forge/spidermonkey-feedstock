d6637b7590a7151391cbe07abd471a8770ce7454
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-10-30 08:37:46 -0500

    try more flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,30 +4,29 @@ export AWK=awk
 export LLVM_OBJDUMP=objdump
 export CPPFLAGS="-D__STDC_FORMAT_MACROS $CPPFLAGS"
 
-if [[ "$(uname)" == "Darwin" ]]; then
-       # ERROR: --with-system-icu is not supported with bootstrapped sysroot. Drop the option, or use --without-sysroot or --disable-bootstrap
-       export CONFIG_PY_ARGS=""
-else
-       export CONFIG_PY_ARGS="--with-system-icu"
-fi
-
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 # We can't build in js/src/, so create a build dir
 mkdir obj
 cd obj/
 python ../configure.py \
        --prefix=$PREFIX \
-       --enable-project=js \
-       --disable-ctypes \
-       --disable-jit \
-       --disable-jemalloc \
-       --enable-optimize \
-       --enable-hardening \
-       --with-intl-api \
        --build-backends=RecursiveMake \
+       --disable-bootstrap \
        --disable-debug \
-       --enable-gczeal \
-       --enable-strip \
+       --disable-debug-symbols \
        --disable-install-strip \
-       $CONFIG_PY_ARGS
+       --disable-jemalloc \
+       --disable-jit \
+       --enable-gczeal \
+       --enable-hardening \
+       --enable-optimize="${CFLAGS} -O2" \
+       --enable-project=js \
+       --enable-release \
+       --enable-rust-simd \
+       --enable-shared-js \
+       --enable-strip \
+       --with-intl-api \
+       --with-system-icu \
+       --with-system-zlib \
+       --with-system-nspr
 make # TOOD: restore -j$CPU_COUNT
